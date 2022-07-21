caadc58baba8f3c11c964c7ffc08c7833df960c6
William Plays0402 <49759911+WilliamPlays0402@users.noreply.github.com>
2022-07-21 14:35:10 +0200

    Update time.js

--- time.js
+++ time.js
@@ -73,72 +73,72 @@ var x = setInterval(() => {
 //     };
 // }, 1);
 
-var time_school = new Date("July 1, 2022 11:00:00").getTime();
-var x = setInterval(() => {
-    var now = new Date().getTime();
-    var distance = time_school - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    var milliseconds = Math.floor((distance % 1000));
-    if (milliseconds <= 9) {
-        milliseconds = "00" + milliseconds;
-    } else if (milliseconds <= 99) {
-        milliseconds = "0" + milliseconds;
-    } else if (milliseconds == 1000) {
-        milliseconds = 999;
-    };
-    if (seconds <= 9) {
-        seconds = "0" + seconds;
-    };
-    if (minutes <= 9) {
-        minutes = "0" + minutes;
-    };
-    if (hours <= 9) {
-        hours = "0" + hours;
-    }
-    // if (milliseconds <= 999) {
-    //     milliseconds = "0" + milliseconds
-    // }
-    document.getElementById("time_school").innerHTML = days + "d " + hours + "h "+ minutes + "m " + seconds + "s " + milliseconds + "ms";
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time_school").innerHTML = "EXPIRED";
-    };
-}, 1);
+// var time_school = new Date("July 1, 2022 11:00:00").getTime();
+// var x = setInterval(() => {
+//     var now = new Date().getTime();
+//     var distance = time_school - now;
+//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//     var milliseconds = Math.floor((distance % 1000));
+//     if (milliseconds <= 9) {
+//         milliseconds = "00" + milliseconds;
+//     } else if (milliseconds <= 99) {
+//         milliseconds = "0" + milliseconds;
+//     } else if (milliseconds == 1000) {
+//         milliseconds = 999;
+//     };
+//     if (seconds <= 9) {
+//         seconds = "0" + seconds;
+//     };
+//     if (minutes <= 9) {
+//         minutes = "0" + minutes;
+//     };
+//     if (hours <= 9) {
+//         hours = "0" + hours;
+//     }
+//     // if (milliseconds <= 999) {
+//     //     milliseconds = "0" + milliseconds
+//     // }
+//     document.getElementById("time_school").innerHTML = days + "d " + hours + "h "+ minutes + "m " + seconds + "s " + milliseconds + "ms";
+//     if (distance < 0) {
+//         clearInterval(x);
+//         document.getElementById("time_school").innerHTML = "EXPIRED";
+//     };
+// }, 1);
 
-var time_activity = new Date("June 27, 2022 08:25:00").getTime();
-var x = setInterval(() => {
-    var now = new Date().getTime();
-    var distance = time_activity - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    var milliseconds = Math.floor((distance % 1000));
-    if (milliseconds <= 9) {
-        milliseconds = "00" + milliseconds;
-    } else if (milliseconds <= 99) {
-        milliseconds = "0" + milliseconds;
-    } else if (milliseconds == 1000) {
-        milliseconds = 999;
-    };
-    if (seconds <= 9) {
-        seconds = "0" + seconds;
-    }
-    if (minutes <= 9) {
-        minutes = "0" + minutes;
-    };
-    if (hours <= 9) {
-        hours = "0" + hours;
-    }
-    document.getElementById("time_activity").innerHTML = days + "d " + hours + "h "+ minutes + "m " + seconds + "s " + milliseconds + "ms";
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("time_activity").innerHTML = "EXPIRED";
-    };
-}, 1);
+// var time_activity = new Date("June 27, 2022 08:25:00").getTime();
+// var x = setInterval(() => {
+//     var now = new Date().getTime();
+//     var distance = time_activity - now;
+//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//     var milliseconds = Math.floor((distance % 1000));
+//     if (milliseconds <= 9) {
+//         milliseconds = "00" + milliseconds;
+//     } else if (milliseconds <= 99) {
+//         milliseconds = "0" + milliseconds;
+//     } else if (milliseconds == 1000) {
+//         milliseconds = 999;
+//     };
+//     if (seconds <= 9) {
+//         seconds = "0" + seconds;
+//     }
+//     if (minutes <= 9) {
+//         minutes = "0" + minutes;
+//     };
+//     if (hours <= 9) {
+//         hours = "0" + hours;
+//     }
+//     document.getElementById("time_activity").innerHTML = days + "d " + hours + "h "+ minutes + "m " + seconds + "s " + milliseconds + "ms";
+//     if (distance < 0) {
+//         clearInterval(x);
+//         document.getElementById("time_activity").innerHTML = "EXPIRED";
+//     };
+// }, 1);
 
 var time_school_start = new Date("September 2, 2022 08:05:00").getTime();
 var x_start = setInterval(() => {
